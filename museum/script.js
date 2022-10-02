ff5d403690e611269cd80b673564481066f67044
marcoscotuquense <114877374+StarDust-official@users.noreply.github.com>
2022-10-02 16:55:17 -0300

    committed the website

---
+++ museum/script.js
@@ -0,0 +1,9 @@
+function Mostrar(id) {
+    let block = document.querySelector(id);
+    block.style.display = "block";
+}
+
+function Fechar(id) {
+    let block = document.querySelector(id);
+    block.style.display = "none";
+}
